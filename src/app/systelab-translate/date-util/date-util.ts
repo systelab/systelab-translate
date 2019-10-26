a27a880556089219db92d4c6feff3ab642784c40
alfonsserra <alfonsoserra@gmail.com>
2019-10-26 09:18:01 +0200

    Leave it as it is in master

--- src/app/systelab-translate/date-util/date-util.ts
+++ src/app/systelab-translate/date-util/date-util.ts
@@ -1,4 +1,4 @@
-import { format, setHours, setMilliseconds, setMinutes, setSeconds } from 'date-fns';
+import { addDays, format, setHours, setMilliseconds, setMinutes, setSeconds } from 'date-fns';
 
 export class DateUtil {
 
@@ -9,12 +9,22 @@ export class DateUtil {
 		this.locale = lang;
 	}
 
+	public getTimeFormat(withSeconds = false): string {
+		const time = (withSeconds) ? (this.locale === 'en-US') ? 'hh:mm:ss a' : 'HH:mm:ss' :
+			(this.locale === 'en-US') ? 'hh:mm a' : 'HH:mm' ;
+		return time;
+	}
+
+	// DATE TIME FUNCTIONS
+
 	public formatDate(date: Date): string {
 		return format(date, this.getDateFormat());
+
 	}
 
 	public formatDateFullYear(date: Date): string {
 		return format(date, this.getDateFormat(true));
+
 	}
 
 	public formatTime(date: Date, withSeconds?: boolean): string {
@@ -25,10 +35,6 @@ export class DateUtil {
 		const formatedDate = (fullYear) ? this.formatDateFullYear(date) : this.formatDate(date);
 		const formatedHour = this.formatTime(date, withSeconds);
 		return formatedDate + ' ' + formatedHour;
-	}
-
-	public formatMonthAndYear(date: Date) {
-		return format(date, 'MMMM, YYYY', {locale: this.locale});
 	}
 
 	public getDateFrom(date: Date) {
@@ -55,11 +61,41 @@ export class DateUtil {
 		return d;
 	}
 
+	public formatMonthAndYear(date: Date) {
+		return format(date, 'MMMM, YYYY', {locale: this.locale});
+	}
+
 	public getDateFormat(isFullYear = false): string {
 		let stringDateFormat = '';
 		switch (this.locale) {
 			case 'en-US':
 				stringDateFormat = 'M/D/YY';
+				break;
+			case 'en-GB':
+			case 'it-IT':
+			case 'es-AR':
+			case 'es-ES':
+			case 'es-BO':
+			case 'es-CL':
+			case 'es-CO':
+			case 'es-CR':
+			case 'es-DO':
+			case 'es-EC':
+			case 'es-GT':
+			case 'es-HN':
+			case 'es-MX':
+			case 'es-NI':
+			case 'es-PA':
+			case 'es-PE':
+			case 'es-PR':
+			case 'es-PY':
+			case 'es-SV':
+			case 'es-UR':
+			case 'es-VE':
+			case 'fr-FR':
+			case 'gl-GL':
+			case 'ca-CA':
+				stringDateFormat = 'DD/MM/YY';
 				break;
 			case 'ko-KO':
 				stringDateFormat = 'YY. M. D';
@@ -89,6 +125,26 @@ export class DateUtil {
 			case 'ja-JA':
 				stringDateFormat = 'YY/MM/DD';
 				break;
+			default:
+				stringDateFormat = 'DD/MM/YY';
+				break;
+		}
+		if (isFullYear) {
+			stringDateFormat = stringDateFormat.replace('YY', 'YYYY');
+			if (this.locale === 'en-US') {
+				stringDateFormat = stringDateFormat.replace('M', 'MM');
+				stringDateFormat = stringDateFormat.replace('D', 'DD');
+			}
+		}
+		return stringDateFormat;
+	}
+
+	public getDateFormatForDatePicker(isFullYear = false): string {
+		let stringDateFormat = '';
+		switch (this.locale) {
+			case 'en-US':
+				stringDateFormat = 'm/d/y';
+				break;
 			case 'en-GB':
 			case 'it-IT':
 			case 'es-AR':
@@ -113,25 +169,7 @@ export class DateUtil {
 			case 'fr-FR':
 			case 'gl-GL':
 			case 'ca-CA':
-			default:
-				stringDateFormat = 'DD/MM/YY';
-				break;
-		}
-		if (isFullYear) {
-			stringDateFormat = stringDateFormat.replace('YY', 'YYYY');
-			if (this.locale === 'en-US') {
-				stringDateFormat = stringDateFormat.replace('M', 'MM');
-				stringDateFormat = stringDateFormat.replace('D', 'DD');
-			}
-		}
-		return stringDateFormat;
-	}
-
-	public getDateFormatForDatePicker(isFullYear = false): string {
-		let stringDateFormat = '';
-		switch (this.locale) {
-			case 'en-US':
-				stringDateFormat = 'm/d/y';
+				stringDateFormat = 'dd/mm/y';
 				break;
 			case 'ko-KO':
 				stringDateFormat = 'y. m. d';
@@ -161,30 +199,6 @@ export class DateUtil {
 			case 'ja-JA':
 				stringDateFormat = 'y/mm/dd';
 				break;
-			case 'en-GB':
-			case 'it-IT':
-			case 'es-AR':
-			case 'es-ES':
-			case 'es-BO':
-			case 'es-CL':
-			case 'es-CO':
-			case 'es-CR':
-			case 'es-DO':
-			case 'es-EC':
-			case 'es-GT':
-			case 'es-HN':
-			case 'es-MX':
-			case 'es-NI':
-			case 'es-PA':
-			case 'es-PE':
-			case 'es-PR':
-			case 'es-PY':
-			case 'es-SV':
-			case 'es-UR':
-			case 'es-VE':
-			case 'fr-FR':
-			case 'gl-GL':
-			case 'ca-CA':
 			default:
 				stringDateFormat = 'dd/mm/y';
 				break;
@@ -199,14 +213,6 @@ export class DateUtil {
 		return stringDateFormat;
 	}
 
-	public getTimeFormat(withSeconds = false): string {
-		if (withSeconds) {
-			return this.locale === 'en-US' ? 'hh:mm:ss a' : 'HH:mm:ss'
-		} else {
-			return this.locale === 'en-US' ? 'hh:mm a' : 'HH:mm';
-		}
-	}
-
 	public getFirstDayOfWeek(): number {
 		switch (this.locale) {
 			case 'en-US':
@@ -219,74 +225,25 @@ export class DateUtil {
 		}
 	}
 
-	public getSeparator(locale: string): string {
-		switch (locale) {
-			case 'pl-PL':
-			case 'lt-LT':
-			case 'pt-PT':
-			case 'pt-BR':
-			case 'nl-NL':
-				return '-';
-			case 'sk-SK':
-			case 'ru-RU':
-			case 'de-DE':
-				return '.';
-			case 'en-US':
-			case 'en-GB':
-			case 'it-IT':
-			case 'es-AR':
-			case 'es-ES':
-			case 'es-BO':
-			case 'es-CL':
-			case 'es-CO':
-			case 'es-CR':
-			case 'es-DO':
-			case 'es-EC':
-			case 'es-GT':
-			case 'es-HN':
-			case 'es-MX':
-			case 'es-NI':
-			case 'es-PA':
-			case 'es-PE':
-			case 'es-PR':
-			case 'es-PY':
-			case 'es-SV':
-			case 'es-UR':
-			case 'es-VE':
-			case 'fr-FR':
-			case 'gl-GL':
-			case 'ca-CA':
-			case 'zh-CN':
-			case 'th-TH':
-			case 'ja-JA':
-			default:
-				return '/';
-		}
-	}
-
 	public parseDate(currentDateValue: string, locale?: string): Date {
+		let auxDate: string, auxArray: Array<string>;
+
+		if (!currentDateValue) {
+			currentDateValue = '';
+		}
 
 		if (!locale) {
 			locale = this.locale;
 		}
 
-		if (!currentDateValue) {
-			return undefined;
-		}
-
-		const auxArray = currentDateValue.split(this.getSeparator(locale));
-		if (auxArray.length!==3) {
-			return undefined;
-		}
-
 		switch (locale) {
 			case 'en-US':
-			case 'ja-JA':
-			case 'zh-CN':
-				return new Date(currentDateValue);
-			case 'pl-PL':
-			case 'lt-LT':
-				return new Date(auxArray.join('/'));
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = currentDateValue;
+				break;
 			case 'en-GB':
 			case 'it-IT':
 			case 'es-AR':
@@ -311,15 +268,72 @@ export class DateUtil {
 			case 'fr-FR':
 			case 'gl-GL':
 			case 'ca-CA':
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
+				break;
+			case 'pl-PL':
+			case 'lt-LT':
+				auxArray = currentDateValue.split('-');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray.join('/');
+				break;
 			case 'pt-PT':
 			case 'pt-BR':
 			case 'nl-NL':
+				auxArray = currentDateValue.split('-');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
+				break;
 			case 'sk-SK':
 			case 'ru-RU':
-			case 'th-TH':
+				auxArray = currentDateValue.split('.');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
+				break;
+			case 'zh-CN':
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = currentDateValue;
+				break;
 			case 'de-DE':
+				auxArray = currentDateValue.split('.');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
+				break;
+			case 'th-TH':
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
+				break;
+			case 'ja-JA':
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = currentDateValue;
+				break;
 			default:
-				return new Date(auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2]);
+				auxArray = currentDateValue.split('/');
+				if (auxArray.indexOf('') > -1) {
+					return undefined;
+				}
+				auxDate = auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2];
 		}
+		return new Date(auxDate);
 	}
 }
