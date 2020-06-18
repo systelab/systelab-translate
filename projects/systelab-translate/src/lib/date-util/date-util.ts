da0710f13dacececd221736750d6b1b25ed41271
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2020-06-18 15:17:39 +0200

    Update date-fns to 2.14

--- projects/systelab-translate/src/lib/date-util/date-util.ts
+++ projects/systelab-translate/src/lib/date-util/date-util.ts
@@ -216,7 +216,7 @@ export class DateUtil {
 		}
 	}
 
-	public convertSystelabLocaleToDateFnsLocale(locale: string): Locale {
+	private convertSystelabLocaleToDateFnsLocale(locale: string): Locale {
 		switch (locale) {
 			case 'es':
 			case 'es-ES':
