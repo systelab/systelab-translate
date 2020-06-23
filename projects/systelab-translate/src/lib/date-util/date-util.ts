2b6bf00d7d5e6359f340048143027fcff03a7362
Carlos Rodríguez Aguado <carlos.rodriguez.aguado@gmail.com>
2020-06-23 11:27:40 +0200

    Update date-fns to 2.14 (#38)
    * Update date-fns to 2.14 (#38)
    * Refactor convertSystelabLocaleToDateFnsLocale to use map instead of a large switch

--- projects/systelab-translate/src/lib/date-util/date-util.ts
+++ projects/systelab-translate/src/lib/date-util/date-util.ts
@@ -1,4 +1,5 @@
 import { format, setHours, setMilliseconds, setMinutes, setSeconds } from 'date-fns';
+import { ca, de, enGB, enUS, es, fr, gl, it, ja, ko, lt, nl, pl, pt, ru, sk, th, zhCN } from 'date-fns/locale';
 
 export class DateUtil {
 
@@ -52,12 +53,12 @@ export class DateUtil {
 	}
 
 	public formatMonthAndYear(date: Date) {
-		return format(date, 'MMMM, YYYY', {locale: this.locale});
+		return format(date, 'MMMM, yyyy', {locale: this.convertSystelabLocaleToDateFnsLocale(this.locale)});
 	}
 
 	public getTimeFormat(withSeconds = false): string {
 		if (withSeconds) {
-			return this.locale === 'en-US' ? 'hh:mm:ss a' : 'HH:mm:ss'
+			return this.locale === 'en-US' ? 'hh:mm:ss a' : 'HH:mm:ss';
 		} else {
 			return this.locale === 'en-US' ? 'hh:mm a' : 'HH:mm';
 		}
@@ -67,45 +68,45 @@ export class DateUtil {
 		let stringDateFormat = '';
 		switch (this.locale) {
 			case 'en-US':
-				stringDateFormat = 'M/D/YY';
+				stringDateFormat = 'M/d/yy';
 				break;
 			case 'ko-KO':
-				stringDateFormat = 'YY. M. D';
+				stringDateFormat = 'yy. M. d';
 				break;
 			case 'pl-PL':
 			case 'lt-LT':
-				stringDateFormat = 'YY-MM-DD';
+				stringDateFormat = 'yy-MM-dd';
 				break;
 			case 'pt-PT':
 			case 'pt-BR':
 			case 'nl-NL':
-				stringDateFormat = 'DD-MM-YY';
+				stringDateFormat = 'dd-MM-yy';
 				break;
 			case 'sk-SK':
 			case 'ru-RU':
-				stringDateFormat = 'D.M.YY';
+				stringDateFormat = 'd.M.yy';
 				break;
 			case 'zh-ZH':
-				stringDateFormat = 'YY-M-D';
+				stringDateFormat = 'yy-M-d';
 				break;
 			case 'de-DE':
-				stringDateFormat = 'DD.MM.YY';
+				stringDateFormat = 'dd.MM.yy';
 				break;
 			case 'th-TH':
-				stringDateFormat = 'D/M/YY';
+				stringDateFormat = 'd/M/yy';
 				break;
 			case 'ja-JA':
-				stringDateFormat = 'YY/MM/DD';
+				stringDateFormat = 'yy/MM/dd';
 				break;
 			default:
-				stringDateFormat = 'DD/MM/YY';
+				stringDateFormat = 'dd/MM/yy';
 				break;
 		}
 		if (isFullYear) {
-			stringDateFormat = stringDateFormat.replace('YY', 'YYYY');
+			stringDateFormat = stringDateFormat.replace('yy', 'yyyy');
 			if (this.locale === 'en-US') {
 				stringDateFormat = stringDateFormat.replace('M', 'MM');
-				stringDateFormat = stringDateFormat.replace('D', 'DD');
+				stringDateFormat = stringDateFormat.replace('D', 'dd');
 			}
 		}
 		return stringDateFormat;
@@ -213,5 +214,19 @@ export class DateUtil {
 			default:
 				return new Date(auxArray[1] + '/' + auxArray[0] + '/' + auxArray[2]);
 		}
+	}
+
+	private convertSystelabLocaleToDateFnsLocale(localeString: string): Locale {
+		const localeForSystelabLocale = new Map<string, Locale>([
+			['es', es], ['es-ES', es], ['es-CL', es], ['es-MX', es], ['es-UY', es], ['es-AR', es], ['es-BO', es],
+			['es-CO', es], ['es-CR', es], ['es-DO', es], ['es-EC', es], ['es-SV', es], ['es-GT', es], ['es-HN', es],
+			['es-NI', es], ['es-PA', es], ['es-PY', es], ['es-PE', es], ['es-PR', es], ['es-VE', es], ['fr', fr],
+			['fr-FR', fr], ['it', it], ['it-IT', it], ['en', enUS], ['en-US', enUS], ['en-GB', enGB], ['pt', pt],
+			['pt-BR', pt], ['ca', ca], ['gl', gl], ['lt', lt], ['lt-LT', lt], ['pl', pl], ['pl-PL', pl], ['sk', sk],
+			['sk-SK', sk], ['ru', ru], ['ru-RU', ru], ['zh', zhCN], ['zh-CN', zhCN], ['de', de], ['de-DE', de],
+			['th', th], ['th-TH', th], ['ja', ja], ['ja-JA', ja], ['ko', ko], ['ko-KR', ko], ['nl', nl], ['nl-NL', nl],
+		]);
+		const locale: Locale = localeForSystelabLocale.get(localeString)
+		return locale ? locale : enUS;
 	}
 }
